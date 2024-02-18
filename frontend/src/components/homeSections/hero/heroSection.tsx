f17cfb08a31a87332b78a358b26cbfe50228a93c
kushagra-aa <kushagraagnihotri24@gmial.com>
2024-02-18 14:54:44 +0530

    added and styles hero

--- frontend/src/components/homeSections/hero/heroSection.tsx
+++ frontend/src/components/homeSections/hero/heroSection.tsx
@@ -1,4 +1,21 @@
+import "./heroSection.css";
+
 function HeroSection() {
-  return <div>HeroSection</div>;
+  return (
+    <section id="hero">
+      <div />
+      <h1>
+        Overcome Investment Barriers,
+        <span />
+        <br />
+        <span />
+        Build <i>Sustainable Wealth</i>
+      </h1>
+      <p>
+        Dive into alternative markets with us and set your capital on the path
+        of <u>unparalleled growth.</u>
+      </p>
+    </section>
+  );
 }
 export default HeroSection;
